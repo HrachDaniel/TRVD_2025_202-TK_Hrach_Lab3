1a1b9a95fd50e047332e56045e3c101b98d23e7a
Hrach Daniyl <daniel.grach06@gmail.com>
2025-11-15 12:41:18 +0200

    +

--- models/book.js
+++ models/book.js
@@ -4,7 +4,11 @@ const bookSchema = new mongoose.Schema({
     title: { type: String, required: true },
     author: { type: String, required: true },
     genre: { type: String },
-    image: { type: String, required: true }
+    image: { type: String, required: true },
+    tags: [String] ,
+    release: { type: String },
+    score: { type: String, required: true },
+    description: { type: String }
 });
 
 const Book = mongoose.model('Book', bookSchema);
